041bd57024a5330fbc4565fd760291753afacec6
NejcKrasna <nejc.krasna@tscng.org>
2025-11-01 13:01:50 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,32 @@
 var createGumb = document.getElementById("create");
+var signinGumb = document.getElementById("signin");
 var closesignup1x = document.getElementById("close_signup1");
 var closesignup2x = document.getElementById("close_signup2");
+var closesigninx = document.getElementById("close_signin");
 var tosignup2 = document.getElementById("tosign2");
 var tofini = document.getElementById("tofin");
+var login_gumbek = document.getElementById("login_gumb");
 var signup_div = document.getElementById("signup_overlay1");
 var signup_div2 = document.getElementById("signup_overlay2");
+var signin_div = document.getElementById("signin_overlay");
 var black_overlay_div = document.getElementById("black_overlay");
 
 closesignup1x.addEventListener("click", closesignup1);
 closesignup2x.addEventListener("click", closesignup2);
+closesigninx.addEventListener("click", closesignin);
 createGumb.addEventListener("click", CreateAccount);
+signinGumb.addEventListener("click", Signin);
 tosignup2.addEventListener("click", tosignup2f);
 tofini.addEventListener("click", tofinish);
+login_gumbek.addEventListener("click", login);
 
 function CreateAccount(){
 	signup_div.style.display = "block";
+	black_overlay_div.style.display = "block";
+}
+
+function Signin(){
+	signin_div.style.display = "block";
 	black_overlay_div.style.display = "block";
 }
 
@@ -25,6 +37,11 @@ function closesignup1(){
 
 function closesignup2(){
 	signup_div2.style.display = "none";
+	black_overlay_div.style.display = "none";
+}
+
+function closesignin(){
+	signin_div.style.display = "none";
 	black_overlay_div.style.display = "none";
 }
 
@@ -137,6 +154,24 @@ function tofinish(){
 	}
 }
 
+function login(){
+	if(document.forms["signin_form"]["inusername"].value=="admin" && document.forms["signin_form"]["inpassword"].value=="admin"){
+		Swal.fire({
+		icon: "success",
+		title: "Dobrodošli!",
+		theme: 'auto'
+		});
+		signin_div.style.display = "none";
+		black_overlay_div.style.display = "none";
+	}else{
+		Swal.fire({
+		icon: "error",
+		title: "Oh no",
+		text: "Incorrect username or password",
+		theme: 'auto'
+		});
+	}
+}
 function valid(email) {
     const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return pattern.test(email);
